c05b1a098b19a6e21c921a2f4918f123809f4565
IgorOluic <igoroluic13@gmail.com>
2020-10-07 22:02:39 +0200

    Add gender svgs and text

--- src/components/content/gender/createDonutChart.ts
+++ src/components/content/gender/createDonutChart.ts
@@ -1,32 +1,74 @@
 import * as d3 from "d3";
 
-const createDonutChart = (gender: string, percentage: number) => {
+const createDonutChart = (maleShare: number, femaleShare: number) => {
   let data = [
     {
       gender: "male",
-      percentage: gender === "male" ? percentage : 100 - percentage
+      percentage: maleShare
     },
     {
       gender: "female",
-      percentage: gender === "female" ? percentage : 100 - percentage
+      percentage: femaleShare
     }
   ];
 
-  const svg = d3.select(".donut-svg");
   let width = 200;
   let height = width;
 
-  let oRadius = width / 4;
-  let iRadius = width / 6.66;
+  const svg = d3
+    .select(".donut-svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  svg.selectAll("*").remove();
+
+  svg
+    .append("text")
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "central")
+    .attr("font-family", "sans-serif")
+    .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+  if (maleShare > femaleShare) {
+    svg
+      .append("svg:image")
+      .attr("xlink:href", "/male-sign.svg")
+      .attr("width", 180)
+      .attr("height", 180)
+      .attr("transform", "translate(22, 0)");
+
+    svg
+      .select("text")
+      .text(maleShare + "%")
+      .attr("fill", "lightblue")
+      .style("font-size", 26);
+  } else if (maleShare < femaleShare) {
+    svg
+      .append("svg:image")
+      .attr("xlink:href", "/female-sign.svg")
+      .attr("width", 180)
+      .attr("height", 180)
+      .attr("transform", "translate(10, 33)");
+
+    svg
+      .select("text")
+      .text(femaleShare + "%")
+      .attr("fill", "lightpink")
+      .style("font-size", 26);
+  } else {
+    svg.select("text").text("50%").attr("fill", "black").style("font-size", 26);
+  }
+
+  let oRadius = 55;
+  let iRadius = 37;
 
   const g = svg
-    .select("g")
+    .append("g")
     .attr("transform", `translate(${width / 2}, ${height / 2})`);
 
   const pie = d3
     .pie<{ gender: string; percentage: number }>()
-    .value((data) => data.percentage)
-    .sort(null);
+    .value((data) => data.percentage);
 
   const path = d3.arc<any>().outerRadius(oRadius).innerRadius(iRadius);
 
@@ -42,29 +84,14 @@ const createDonutChart = (gender: string, percentage: number) => {
     .attr("d", path)
     .data(data)
     .attr("fill", (data) => {
+      let color: string;
       if (data.gender === "male") {
-        return "lightblue";
+        color = "lightblue";
       } else {
-        return "lightpink";
+        color = "lightpink";
       }
+      return color;
     });
-
-  d3.select(".gender-icon")
-    .attr("height", oRadius * 4)
-    .attr("width", oRadius * 4)
-    .attr("viewBox", "0 0 600 600");
-
-  if (gender === "male") {
-    d3.select(".gender-icon")
-      .attr("fill", "lightblue")
-      .select("path")
-      .attr("transform", "translate(110, -15)");
-  } else if (gender === "female") {
-    d3.select(".gender-icon")
-      .attr("fill", "lightpink")
-      .select("path")
-      .attr("transform", "translate(45, 125)");
-  }
 };
 
 export default createDonutChart;
