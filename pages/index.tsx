288d97420dedaaa6a7deb307392f35e06f1d3829
IgorOluic <igoroluic13@gmail.com>
2020-10-12 16:02:15 +0200

    Display existing components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Button, Input, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import DisplayGender from "../src/components/content/gender/DisplayGender";
+import DisplayCountries from "../src/components/content/countries/DisplayCountries";
 
 const useStyles = makeStyles({
   root: {
@@ -21,8 +23,11 @@ export default function Home(): JSX.Element {
         style={{ height: "100px" }}
       >
         <Input
-          placeholder="Placeholder"
-          inputProps={{ "aria-label": "description" }}
+          placeholder="Type a name here..."
+          inputProps={{
+            "aria-label": "description",
+            style: { textAlign: "center" }
+          }}
         />
       </Grid>
       <Grid
@@ -44,14 +49,30 @@ export default function Home(): JSX.Element {
         alignItems="center"
         style={{ height: "600px" }}
       >
+        <Grid item style={{ backgroundColor: "#556cd6", textAlign: "center" }}>
+          <DisplayGender maleShare={90} />
+          <DisplayCountries
+            countries={[
+              {
+                countryName: "Serbia",
+                namePopularity: 0.88823
+              },
+              {
+                countryName: "Russia",
+                namePopularity: 0.847328
+              },
+              {
+                countryName: "Belarus",
+                namePopularity: 0.52323
+              }
+            ]}
+          />
+        </Grid>
         <Grid item>
           <p>MAP</p>
         </Grid>
         <Grid item>
           <p>GLOBE</p>
-        </Grid>
-        <Grid item>
-          <p>CONTENT</p>
         </Grid>
       </Grid>
     </Grid>
