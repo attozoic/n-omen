c29bff24c49fe5327d30f7ccf7223ff98d60ca00
DESKTOP-5GNAQRK\Tatjana <devrnjat94@gmail.com>
2020-10-13 16:50:01 +0200

    Rename component and change props

--- src/components/content/age/Age.tsx
+++ src/components/content/age/Age.tsx
@@ -1,13 +1,18 @@
 import React, { FC } from "react";
 import { Typography } from "@material-ui/core";
 
-interface DisplayAgeProps {
-  age: number | null;
+interface AgeProps {
+  isLoading: boolean;
+  error: Error | null;
+  data: {
+    age: number | null;
+  };
 }
 
-const DisplayAge: FC<DisplayAgeProps> = (props): JSX.Element => {
-  const { age } = props;
+const Age: FC<AgeProps> = (props): JSX.Element => {
+  const { data } = props;
+  const { age } = data;
   return <Typography>{age}</Typography>;
 };
 
-export default DisplayAge;
+export default Age;
