320a9ea3799cc8ed35bde1c58e7f1d29462f43ae
IgorOluic <igoroluic13@gmail.com>
2020-10-22 10:03:53 +0200

    Add tests for CountryInfo component

---
+++ src/components/content/countries/__tests__/CountryInfo.test.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { shallow } from "enzyme";
+import CountryInfo from "../CountryInfo";
+import { CountryData } from "../types";
+
+const setup = (propsOverride?: { countries: CountryData[] }) => {
+  const props = {
+    countries: [
+      {
+        countryName: "Serbia",
+        namePopularity: 0.88823
+      }
+    ],
+    ...propsOverride
+  };
+
+  const wrapper = shallow(<CountryInfo {...props} />);
+  const countryWrapper = wrapper.find({ "data-testid": "countryWrapper" });
+
+  return { wrapper, countryWrapper };
+};
+
+describe("CountryInfo component", () => {
+  it("should render", () => {
+    const { wrapper } = setup();
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  describe("recieves array of countries", () => {
+    it("should render all countries from an array", () => {
+      const countries = [
+        {
+          countryName: "Serbia",
+          namePopularity: 0.88823
+        },
+        {
+          countryName: "Russia",
+          namePopularity: 0.98382
+        }
+      ];
+      const { wrapper, countryWrapper } = setup({ countries });
+      expect(wrapper.find({ children: "Serbia" }).exists()).toBe(true);
+      expect(wrapper.find({ children: "Russia" }).exists()).toBe(true);
+      expect(countryWrapper.length).toBe(countries.length);
+    });
+  });
+});
