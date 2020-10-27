a2880d381b356849185daf669f99f9fb5bbf4f84
IgorOluic <igoroluic13@gmail.com>
2020-10-27 10:21:04 +0100

    Add GlobeWrapper component

---
+++ src/components/globe/GlobeWrapper.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from "react";
+import { LinearProgress } from "@material-ui/core";
+import Globe from "./Globe";
+
+interface GlobeWrapperProps {
+  isLoading: boolean | null;
+  error: Error | null;
+  data: {
+    countryIds: string[] | null;
+    mapCoords: number[];
+  };
+}
+
+const GlobeWrapper: FC<GlobeWrapperProps> = (props) => {
+  const { data, isLoading, error } = props;
+
+  if (isLoading) {
+    return <LinearProgress />;
+  }
+
+  if (error != null) {
+    return <div>Error component</div>;
+  }
+
+  return <Globe data={data} />;
+};
+
+export default GlobeWrapper;
