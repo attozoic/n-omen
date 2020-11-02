d423922d0580da1c9fb36ee1ec385515b29c60f1
IgorOluic <igoroluic13@gmail.com>
2020-11-02 15:54:39 +0100

    Add coords reducers

---
+++ src/redux/coords/reducers.ts
@@ -0,0 +1,32 @@
+import {
+  UPDATE_GLOBE_COORDS,
+  UPDATE_MAP_COORDS,
+  CoordsType,
+  Coords
+} from "./types";
+
+const defaultState: Coords = {
+  globeCoords: [0, 0],
+  mapCoords: [0, 0]
+};
+
+const coordsReducer = (state = defaultState, action: CoordsType): Coords => {
+  switch (action.type) {
+    case UPDATE_GLOBE_COORDS: {
+      return {
+        ...state,
+        globeCoords: action.globeCoords
+      };
+    }
+    case UPDATE_MAP_COORDS: {
+      return {
+        ...state,
+        mapCoords: action.mapCoords
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default coordsReducer;
