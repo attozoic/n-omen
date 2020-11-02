2820595156e98bbf4783bde9a7ed4065518fba44
IgorOluic <igoroluic13@gmail.com>
2020-11-02 15:58:30 +0100

    Add store in the provider
    Make minor styling changes and change isLoading prop to true

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Button, Input, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { Provider } from "react-redux";
 import Gender from "../src/components/content/gender/Gender";
 import Age from "../src/components/content/age/Age";
 import Country from "../src/components/content/countries/Country";
 import MapWrapper from "../src/components/map/MapWrapper";
 import GlobeWrapper from "../src/components/globe/GlobeWrapper";
+import store from "../src/redux/store/store";
 
 const useStyles = makeStyles({
   root: {
@@ -33,83 +35,85 @@ const useStyles = makeStyles({
 export default function Home(): JSX.Element {
   const classes = useStyles();
   return (
-    <Grid className={classes.root}>
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-        classes={{ root: classes.inputGrid }}
-      >
-        <Input
-          placeholder="Type a name here..."
-          classes={{ root: classes.input }}
-        />
-      </Grid>
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-        classes={{ root: classes.buttonGrid }}
-      >
-        <Button color="primary" variant="contained">
-          Button
-        </Button>
-      </Grid>
+    <Provider store={store}>
+      <Grid className={classes.root}>
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+          classes={{ root: classes.inputGrid }}
+        >
+          <Input
+            placeholder="Type a name here..."
+            classes={{ root: classes.input }}
+          />
+        </Grid>
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+          classes={{ root: classes.buttonGrid }}
+        >
+          <Button color="primary" variant="contained">
+            Button
+          </Button>
+        </Grid>
 
-      <Grid
-        container
-        direction="row"
-        justify="space-around"
-        alignItems="center"
-        classes={{ root: classes.itemsGrid }}
-      >
-        <Grid item classes={{ root: classes.contentGrid }}>
-          <Gender data={{ maleShare: 80 }} isLoading error={null} />
-          <Age data={{ age: 45 }} isLoading error={null} />
-          <Country
-            data={{
-              countries: [
-                {
-                  countryName: "Serbia",
-                  namePopularity: 0.88823
-                },
-                {
-                  countryName: "Russia",
-                  namePopularity: 0.847328
-                },
-                {
-                  countryName: "Belarus",
-                  namePopularity: 0.52323
-                }
-              ]
-            }}
-            isLoading
-            error={null}
-          />
-        </Grid>
-        <Grid xs={3} item>
-          <MapWrapper
-            isLoading={false}
-            error={null}
-            data={{
-              globeCoords: [0, 0],
-              countryIds: ["RS", "HR"]
-            }}
-          />
-        </Grid>
-        <Grid xs={3} item>
-          <GlobeWrapper
-            isLoading={false}
-            error={null}
-            data={{
-              mapCoords: [0, 0],
-              countryIds: ["RS", "HR"]
-            }}
-          />
+        <Grid
+          container
+          direction="row"
+          justify="space-around"
+          alignItems="center"
+          classes={{ root: classes.itemsGrid }}
+        >
+          <Grid xs={3} item classes={{ root: classes.contentGrid }}>
+            <Gender data={{ maleShare: 80 }} isLoading error={null} />
+            <Age data={{ age: 45 }} isLoading error={null} />
+            <Country
+              data={{
+                countries: [
+                  {
+                    countryName: "Serbia",
+                    namePopularity: 0.88823
+                  },
+                  {
+                    countryName: "Russia",
+                    namePopularity: 0.847328
+                  },
+                  {
+                    countryName: "Belarus",
+                    namePopularity: 0.52323
+                  }
+                ]
+              }}
+              isLoading
+              error={null}
+            />
+          </Grid>
+          <Grid xs={3} item>
+            <MapWrapper
+              isLoading
+              error={null}
+              data={{
+                globeCoords: [0, 0],
+                countryIds: ["RS", "HR"]
+              }}
+            />
+          </Grid>
+          <Grid xs={3} item>
+            <GlobeWrapper
+              isLoading
+              error={null}
+              data={{
+                mapCoords: [0, 0],
+                countryIds: ["RS", "HR"]
+              }}
+            />
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </Provider>
   );
 }
