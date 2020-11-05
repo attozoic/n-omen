3b2fa2d24e9f6046de12b0b84a902fac280003ea
IgorOluic <igoroluic13@gmail.com>
2020-11-05 11:25:34 +0100

    Rename isBusy to isLoading and change payload usage

--- src/components/content/state/reducers.ts
+++ src/components/content/state/reducers.ts
@@ -1,26 +1,28 @@
-import { createReducer } from "typesafe-actions";
+import { createReducer } from 'typesafe-actions';
 import {
   GET_NAME_INFO,
   GET_NAME_INFO_FAIL,
   GET_NAME_INFO_SUCCESS
-} from "./actions";
-import initialState from "./initialState";
+} from './actions';
+import initialState from './initialState';
 
 export default createReducer(initialState, {
   [GET_NAME_INFO]: (state) => ({
     ...state,
-    isBusy: true,
+    isLoading: true,
     error: null
   }),
-  [GET_NAME_INFO_FAIL]: (state, { payload }) => ({
+  [GET_NAME_INFO_FAIL]: (state, payload) => ({
     ...state,
-    isBusy: false,
+    isLoading: false,
     error: payload
   }),
   [GET_NAME_INFO_SUCCESS]: (state, { payload }) => ({
     ...state,
-    ...payload,
-    isBusy: false,
+    nameInfo: {
+      ...payload
+    },
+    isLoading: false,
     error: null
   })
 });
