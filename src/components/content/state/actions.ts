de5ba24bc643704d137205e7ffbeb2706f76cc40
IgorOluic <igoroluic13@gmail.com>
2020-11-05 11:27:56 +0100

    Replace deprecated functions and update the new ones

--- src/components/content/state/actions.ts
+++ src/components/content/state/actions.ts
@@ -1,13 +1,12 @@
-import { createStandardAction } from "typesafe-actions/dist/deprecated/create-standard-action";
+import { createAction } from 'typesafe-actions';
 
-export const GET_NAME_INFO = "GET_NAME_INFO";
-export const GET_NAME_INFO_SUCCESS = "GET_NAME_INFO_SUCCESS";
-export const GET_NAME_INFO_FAIL = "GET_NAME_INFO_FAIL";
+export const GET_NAME_INFO = 'GET_NAME_INFO';
+export const GET_NAME_INFO_SUCCESS = 'GET_NAME_INFO_SUCCESS';
+export const GET_NAME_INFO_FAIL = 'GET_NAME_INFO_FAIL';
 
-export const getNameInfo = createStandardAction(GET_NAME_INFO)<undefined>();
-export const getNameInfoSuccess = createStandardAction(GET_NAME_INFO_SUCCESS)<
-  boolean
->();
-export const getNameInfoFail = createStandardAction(GET_NAME_INFO_FAIL)<
-  Error
->();
+export const getNameInfo = createAction(GET_NAME_INFO);
+export const getNameInfoSuccess = createAction(
+  GET_NAME_INFO_SUCCESS,
+  (data) => data
+)();
+export const getNameInfoFail = createAction(GET_NAME_INFO_FAIL)<Error>();
