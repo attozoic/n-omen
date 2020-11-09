37c53daf309ef63956e7626f92c6c631fe49b75e
IgorOluic <igoroluic13@gmail.com>
2020-11-09 11:06:42 +0100

    Add locations to initialState and AppState type

--- src/state/initialState.ts
+++ src/state/initialState.ts
@@ -1,12 +1,16 @@
 import content from '../components/content/state/initialState';
+import locations from '../components/locations/state/initialState';
 import { ContentState } from '../components/content/state/types';
+import { LocationsState } from '../components/locations/state/types';
 
 export type AppState = {
   content: ContentState;
+  locations: LocationsState;
 };
 
 const initialState = {
-  content
+  content,
+  locations
 };
 
 export default initialState;
