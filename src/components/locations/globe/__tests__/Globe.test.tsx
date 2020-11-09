5a758242d8d77ba1116e02ec5841684f6b149d1e
IgorOluic <igoroluic13@gmail.com>
2020-11-09 11:14:49 +0100

    Rename mapCoords to coords and add updateCoords to props

--- src/components/locations/globe/__tests__/Globe.test.tsx
+++ src/components/locations/globe/__tests__/Globe.test.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Globe from '../Globe';
+import { updateCoords } from '../../state/actions';
 
 const setup = () => {
   const props = {
     data: {
       countryIds: ['RS', 'HR'],
-      mapCoords: [0, 0]
-    }
+      coords: [0, 0]
+    },
+    updateCoords
   };
 
   const wrapper = shallow(<Globe {...props} />);
