5e938be4f3eee878eb07b3dd314996266c7663f3
IgorOluic <igoroluic13@gmail.com>
2020-11-12 16:00:47 +0100

    Replace old Search component with material-ui appBar

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,51 +1,120 @@
 import React, { FC } from 'react';
-import { Button, Input, Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles({
-  input: {
-    textAlign: 'center'
-  },
-  inputGrid: {
-    height: '100px'
-  },
-  buttonGrid: {
-    height: '5px'
-  }
-});
+import {
+  makeStyles,
+  fade,
+  createStyles,
+  Theme
+} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
 
 export interface SearchProps {
   getNameInfo: () => void;
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    menuButton: {
+      marginRight: theme.spacing(2)
+    },
+    title: {
+      flexGrow: 1,
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block'
+      }
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25)
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto'
+      }
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    inputRoot: {
+      color: 'inherit'
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch'
+        }
+      }
+    }
+  })
+);
+
 const Search: FC<SearchProps> = ({ getNameInfo }): JSX.Element => {
   const classes = useStyles();
+
+  const handleKeyDown = (event: { key: string }) => {
+    if (event.key === 'Enter') {
+      getNameInfo();
+    }
+  };
+
   return (
-    <div>
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-        classes={{ root: classes.inputGrid }}
-      >
-        <Input
-          placeholder="Type a name here..."
-          classes={{ root: classes.input }}
-          id="search-input"
-        />
-      </Grid>
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-        classes={{ root: classes.buttonGrid }}
-      >
-        <Button color="primary" variant="contained" onClick={getNameInfo}>
-          Search
-        </Button>
-      </Grid>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Material-UI
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              onKeyDown={handleKeyDown}
+              id="search-input"
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
