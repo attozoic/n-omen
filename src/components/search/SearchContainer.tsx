774119b2b7633b35fd74b787516dabb17cfcb769
IgorOluic <igoroluic13@gmail.com>
2020-11-16 15:13:20 +0100

    Add toggleMobileOpen to mapDispatch

--- src/components/search/SearchContainer.tsx
+++ src/components/search/SearchContainer.tsx
@@ -1,11 +1,12 @@
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import Search, { SearchProps } from './Search';
-import { GET_NAME_INFO } from '../../state/actions';
+import { GET_NAME_INFO, TOGGLE_MOBILE_OPEN } from '../../state/actions';
 
 export const mapDispatchToProps = (dispatch: Dispatch): SearchProps => {
   return {
-    getNameInfo: () => dispatch({ type: GET_NAME_INFO })
+    getNameInfo: () => dispatch({ type: GET_NAME_INFO }),
+    toggleMobileOpen: () => dispatch({ type: TOGGLE_MOBILE_OPEN })
   };
 };
 
