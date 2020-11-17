0acdf138d5e63cdc205c115b394c19f8af917c4b
IgorOluic <igoroluic13@gmail.com>
2020-11-17 15:38:32 +0100

    Add tests for TOGGLE_MOBILE_OPEN and GET_NAME_INFO

--- src/components/content/state/__tests__/reducers.test.ts
+++ src/components/content/state/__tests__/reducers.test.ts
@@ -1,11 +1,26 @@
 import reducer from '../reducers';
 import initialState from '../initialState';
-import { GET_NAME_INFO_FAIL, GET_NAME_INFO_SUCCESS } from '../actions';
+import {
+  GET_NAME_INFO_FAIL,
+  GET_NAME_INFO_SUCCESS,
+  GET_NAME_INFO,
+  TOGGLE_MOBILE_OPEN
+} from '../actions';
 
 describe('Content reducers', () => {
   it('should return the initial state', () => {
     expect(reducer(undefined, {})).toEqual(initialState);
   });
+
+  it('should handle GET_NAME_INFO', () =>
+    expect(
+      reducer(initialState, {
+        type: GET_NAME_INFO
+      })
+    ).toEqual({
+      ...initialState,
+      isLoading: true
+    }));
 
   it('should handle GET_NAME_INFO_FAIL', () =>
     expect(
@@ -44,5 +59,15 @@ describe('Content reducers', () => {
         maleShare: 22,
         name: 'John'
       }
+    }));
+
+  it('should handle TOGGLE_MOBILE_OPEN', () =>
+    expect(
+      reducer(initialState, {
+        type: TOGGLE_MOBILE_OPEN
+      })
+    ).toEqual({
+      ...initialState,
+      mobileOpen: !initialState.mobileOpen
     }));
 });
