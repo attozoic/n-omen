b269e74951237fa344a04445d7f6d729c80aa491
IgorOluic <igoroluic13@gmail.com>
2020-11-20 17:12:53 +0100

    Add updateCentroid action

--- src/components/locations/state/actions.ts
+++ src/components/locations/state/actions.ts
@@ -1,8 +1,14 @@
 import { createAction } from 'typesafe-actions';
 
 export const UPDATE_COORDS = 'UPDATE_COORDS';
+export const UPDATE_CENTROID = 'UPDATE_CENTROID';
 
 export const updateCoords = createAction(
   UPDATE_COORDS,
   (coords: number[]) => coords
+)<number[]>();
+
+export const updateCentroid = createAction(
+  UPDATE_CENTROID,
+  (centroid: number[]) => centroid
 )<number[]>();
