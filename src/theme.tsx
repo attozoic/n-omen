0413fdf1ceb715de5ebc56ddbc59ebe7a9875455
IgorOluic <igoroluic13@gmail.com>
2021-01-06 18:27:36 +0100

    Update primary color

--- src/theme.tsx
+++ src/theme.tsx
@@ -3,7 +3,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6'
+      main: '#0abf40'
     },
     secondary: {
       main: '#19857b'
