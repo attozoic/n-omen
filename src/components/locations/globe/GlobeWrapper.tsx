efecb6cacf50191ba39dc0e6d02cf4cda1efc0fe
IgorOluic <igoroluic13@gmail.com>
2021-01-06 18:03:15 +0100

    Remove unused conditionals and props

--- src/components/locations/globe/GlobeWrapper.tsx
+++ src/components/locations/globe/GlobeWrapper.tsx
@@ -1,24 +1,13 @@
 import React, { FC } from 'react';
-import { LinearProgress } from '@material-ui/core';
 import Globe from './Globe';
 
 export interface GlobeWrapperProps {
-  isLoading: boolean | null;
-  error: Error | null;
   data: {
     coords: number[];
   };
 }
 
-const GlobeWrapper: FC<GlobeWrapperProps> = ({ data, isLoading, error }) => {
-  if (isLoading) {
-    return <LinearProgress />;
-  }
-
-  if (error != null) {
-    return <div>Error component</div>;
-  }
-
+const GlobeWrapper: FC<GlobeWrapperProps> = ({ data }) => {
   return <Globe data={data} />;
 };
 
