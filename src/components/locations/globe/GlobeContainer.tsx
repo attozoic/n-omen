c7a3abe4b7741166447d88a9a2ab406f320b099d
IgorOluic <igoroluic13@gmail.com>
2021-01-06 18:03:41 +0100

    Remove unused props

--- src/components/locations/globe/GlobeContainer.tsx
+++ src/components/locations/globe/GlobeContainer.tsx
@@ -3,15 +3,12 @@ import { AppState } from '../../../state/initialState';
 import GlobeWrapper, { GlobeWrapperProps } from './GlobeWrapper';
 
 export const mapStateToProps = ({
-  content: { isLoading, error },
   locations: { coords }
 }: AppState): GlobeWrapperProps => {
   return {
     data: {
       coords
-    },
-    isLoading,
-    error
+    }
   };
 };
 
