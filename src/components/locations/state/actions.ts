3d13093b99206a7c786ede549fa64be179377f2f
IgorOluic <igoroluic13@gmail.com>
2021-01-06 17:56:54 +0100

    Add new export

--- src/components/locations/state/actions.ts
+++ src/components/locations/state/actions.ts
@@ -1,5 +1,6 @@
 import { createAction } from 'typesafe-actions';
 
+export { GET_NAME_INFO_FAIL } from '../../content/state/actions';
 export const UPDATE_COORDS = 'UPDATE_COORDS';
 export const UPDATE_CENTROID = 'UPDATE_CENTROID';
 
