0404c2c3f2e601059f9fd572c0b33bb0cfd9f0ca
IgorOluic <igoroluic13@gmail.com>
2021-01-06 17:57:50 +0100

    Update MapWrapper component tests

--- src/components/locations/map/__tests__/MapWrapper.test.tsx
+++ src/components/locations/map/__tests__/MapWrapper.test.tsx
@@ -1,68 +1,35 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { LinearProgress } from '@material-ui/core';
 import MapWrapper from '../MapWrapper';
-import WorldMap from '../WorldMap';
 import { updateCoords } from '../../state/actions';
 import { mapStateToProps, mapDispatchToProps } from '../MapContainer';
 import initialState from '../../../../state/initialState';
 
-const setup = (propOverrides?: { isLoading: boolean; error: Error }) => {
+const setup = (propOverrides?: { isLoading: boolean }) => {
   const props = {
     data: {
-      countryIds: ['RS', 'HR']
+      countryIds: ['RS', 'HR'],
+      centroid: [0, 0],
+      haveContent: false
     },
     isLoading: true,
     error: null,
+    firstSearchDone: false,
     updateCoords,
     ...propOverrides
   };
 
   const wrapper = shallow(<MapWrapper {...props} />);
-  const mapComponent = wrapper.find(WorldMap);
-  const loadingComponent = wrapper.find(LinearProgress);
-  const errorComponent = wrapper.find({ children: 'Error component' });
 
   return {
-    wrapper,
-    mapComponent,
-    loadingComponent,
-    errorComponent
+    wrapper
   };
 };
 
 describe('MapWrapper component', () => {
-  describe('isLoading is true', () => {
-    it('should render only loading indicator', () => {
-      const { loadingComponent, mapComponent, errorComponent } = setup();
-      expect(loadingComponent.exists()).toBe(true);
-      expect(mapComponent.exists()).toBe(false);
-      expect(errorComponent.exists()).toBe(false);
-    });
-  });
-
-  describe('isLoading is false and there is an error', () => {
-    it('should render only error component', () => {
-      const { loadingComponent, mapComponent, errorComponent } = setup({
-        isLoading: false,
-        error: Error('Error found')
-      });
-      expect(loadingComponent.exists()).toBe(false);
-      expect(mapComponent.exists()).toBe(false);
-      expect(errorComponent.exists()).toBe(true);
-    });
-  });
-
-  describe('isLoading is false and there is no error', () => {
-    it('should render only map component', () => {
-      const { loadingComponent, mapComponent, errorComponent } = setup({
-        isLoading: false,
-        error: null
-      });
-      expect(loadingComponent.exists()).toBe(false);
-      expect(mapComponent.exists()).toBe(true);
-      expect(errorComponent.exists()).toBe(false);
-    });
+  it('should render', () => {
+    const { wrapper } = setup();
+    expect(wrapper.exists()).toBe(true);
   });
 
   describe('mapStateToProps', () => {
@@ -70,9 +37,11 @@ describe('MapWrapper component', () => {
       const testInitialState = {
         ...initialState,
         content: {
+          firstSearchDone: false,
           isLoading: true,
           error: null,
           mobileOpen: false,
+          ...initialState.content,
           nameInfo: {
             countryIds: ['RS', 'HR'],
             name: null,
@@ -80,14 +49,20 @@ describe('MapWrapper component', () => {
             maleShare: null,
             countries: []
           }
+        },
+        locations: {
+          ...initialState.locations,
+          centroid: [25, 25]
         }
       };
 
-      expect(mapStateToProps(testInitialState).isLoading).toBe(true);
-      expect(mapStateToProps(testInitialState).error).toBe(null);
       expect(mapStateToProps(testInitialState).data.countryIds).toStrictEqual([
         'RS',
         'HR'
+      ]);
+      expect(mapStateToProps(testInitialState).data.centroid).toStrictEqual([
+        25,
+        25
       ]);
     });
   });
