02bc9b4f4adccde89d0611b30b556b294e76c86e
IgorOluic <igoroluic13@gmail.com>
2021-01-06 18:09:51 +0100

    Add checkData function

--- src/components/content/state/sagas.ts
+++ src/components/content/state/sagas.ts
@@ -1,10 +1,13 @@
 import { all, put, takeEvery } from 'redux-saga/effects';
 import getNameInfoApi from '../../../data/api/getNameInfoApi';
 import { getNameInfoFail, getNameInfoSuccess, GET_NAME_INFO } from './actions';
+import checkData from '../../../data/api/checkData';
 
 export function* getNameInfo$(): Generator {
   try {
     const response = yield getNameInfoApi();
+
+    checkData(response);
 
     yield put(getNameInfoSuccess(response));
   } catch (error) {
