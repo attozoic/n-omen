d4ba6bfd17479143fa63ebe1c7b9d37607763a5e
IgorOluic <igoroluic13@gmail.com>
2021-01-06 18:14:00 +0100

    Update styling and remove unused conditionals

--- src/components/content/name/Name.tsx
+++ src/components/content/name/Name.tsx
@@ -1,49 +1,34 @@
-import {
-  LinearProgress,
-  Typography,
-  makeStyles,
-  createStyles
-} from '@material-ui/core';
+import { Typography, makeStyles, createStyles, Theme } from '@material-ui/core';
 import React, { FC } from 'react';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    nameText: {
+      [theme.breakpoints.up('xs')]: {
+        color: '#E8E8E8',
+        borderBottom: '4px solid #E8E8E8',
+        textAlign: 'center',
+        fontSize: 35
+      },
+      [theme.breakpoints.up('sm')]: {
+        fontSize: 45,
+        marginTop: '40px',
+        paddingBottom: '40px'
+      }
+    }
+  })
+);
+
 export interface NameProps {
-  isLoading: boolean;
-  error: Error | null;
   data: {
     name: string;
   };
 }
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    nameText: {
-      color: '#fff',
-      fontSize: 30,
-      textAlign: 'center',
-      width: 240,
-      height: 40,
-      borderBottom: '1px solid',
-      backgroundColor: '#4456ab'
-    }
-  })
-);
-
-const Name: FC<NameProps> = ({
-  data: { name },
-  isLoading,
-  error
-}): JSX.Element => {
+const Name: FC<NameProps> = ({ data: { name } }): JSX.Element => {
   const classes = useStyles();
-  if (isLoading) return <LinearProgress />;
 
-  if (error != null) {
-    return <div>Error component</div>;
-  }
-  return (
-    <div>
-      <Typography className={classes.nameText}>{name}</Typography>
-    </div>
-  );
+  return <Typography className={classes.nameText}>{name}</Typography>;
 };
 
 export default Name;
