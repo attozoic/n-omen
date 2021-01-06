c159d6c3f397310d2d20b44a83d351906133c4a0
IgorOluic <igoroluic13@gmail.com>
2021-01-06 18:20:21 +0100

    Update Country component tests

--- src/components/content/countries/__tests__/Country.test.tsx
+++ src/components/content/countries/__tests__/Country.test.tsx
@@ -1,70 +1,32 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { LinearProgress } from '@material-ui/core';
-import CountryInfo from '../CountryInfo';
 import Country from '../Country';
 import { mapStateToProps } from '../CountryContainer';
 import initialState from '../../../../state/initialState';
+import { updateCentroid, updateCoords } from '../../state/actions';
 
-const setup = (propOverrides?: { isLoading: boolean; error: Error }) => {
+const setup = () => {
   const props = {
     data: {
-      countries: [{ countryName: 'Serbia', namePopularity: 0.8237 }]
+      countries: [{ countryName: 'Serbia', namePopularity: 0.8237 }],
+      haveContent: true,
+      mapShouldMove: true
     },
-    isLoading: true,
-    error: null,
-    ...propOverrides
+    updateCentroid,
+    updateCoords
   };
 
   const wrapper = shallow(<Country {...props} />);
-  const countryInfoComponent = wrapper.find(CountryInfo);
-  const loadingComponent = wrapper.find(LinearProgress);
-  const errorComponent = wrapper.find({ children: 'Error component' });
 
   return {
-    wrapper,
-    countryInfoComponent,
-    loadingComponent,
-    errorComponent
+    wrapper
   };
 };
 
 describe('Country component', () => {
-  describe('isLoading is true', () => {
-    it('should render only loading indicator', () => {
-      const {
-        loadingComponent,
-        countryInfoComponent,
-        errorComponent
-      } = setup();
-      expect(loadingComponent.exists()).toBe(true);
-      expect(countryInfoComponent.exists()).toBe(false);
-      expect(errorComponent.exists()).toBe(false);
-    });
-  });
-
-  describe('isLoading is false and there is an error', () => {
-    it('should render only error component', () => {
-      const { loadingComponent, countryInfoComponent, errorComponent } = setup({
-        isLoading: false,
-        error: Error('Error found')
-      });
-      expect(errorComponent.exists()).toBe(true);
-      expect(loadingComponent.exists()).toBe(false);
-      expect(countryInfoComponent.exists()).toBe(false);
-    });
-  });
-
-  describe('isLoading is false and there is no error', () => {
-    it('should render only CountryInfo component', () => {
-      const { loadingComponent, countryInfoComponent, errorComponent } = setup({
-        isLoading: false,
-        error: null
-      });
-      expect(countryInfoComponent.exists()).toBe(true);
-      expect(errorComponent.exists()).toBe(false);
-      expect(loadingComponent.exists()).toBe(false);
-    });
+  it('should render', () => {
+    const { wrapper } = setup();
+    expect(wrapper.exists()).toBe(true);
   });
 
   describe('mapStateToProps', () => {
@@ -73,6 +35,7 @@ describe('Country component', () => {
         ...initialState,
         content: {
           ...initialState.content,
+          haveContent: true,
           nameInfo: {
             ...initialState.content.nameInfo,
             countries: [
@@ -83,8 +46,6 @@ describe('Country component', () => {
         }
       };
 
-      expect(mapStateToProps(testInitialState).isLoading).toBe(true);
-      expect(mapStateToProps(testInitialState).error).toBe(null);
       expect(mapStateToProps(testInitialState).data.countries.length).toBe(2);
       expect(
         mapStateToProps(testInitialState).data.countries[0].countryName
@@ -92,6 +53,7 @@ describe('Country component', () => {
       expect(
         mapStateToProps(testInitialState).data.countries[0].namePopularity
       ).toBe(0.01313);
+      expect(mapStateToProps(testInitialState).data.haveContent).toBe(true);
     });
   });
 });
