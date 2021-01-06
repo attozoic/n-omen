ced13b926429536019fab8174f3b8b3d56b887db
IgorOluic <igoroluic13@gmail.com>
2021-01-06 18:02:30 +0100

    Update GlobeWrapper component tests

--- src/components/locations/globe/__tests__/GlobeWrapper.test.tsx
+++ src/components/locations/globe/__tests__/GlobeWrapper.test.tsx
@@ -1,83 +1,43 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { LinearProgress } from '@material-ui/core';
 import GlobeWrapper from '../GlobeWrapper';
-import Globe from '../Globe';
 import { mapStateToProps } from '../GlobeContainer';
 import initialState from '../../../../state/initialState';
 
-const setup = (propOverrides?: { isLoading: boolean; error: Error }) => {
+const setup = () => {
   const props = {
     data: {
       coords: [0, 0]
-    },
-    isLoading: true,
-    error: null,
-    ...propOverrides
+    }
   };
 
   const wrapper = shallow(<GlobeWrapper {...props} />);
-  const globeComponent = wrapper.find(Globe);
-  const loadingComponent = wrapper.find(LinearProgress);
-  const errorComponent = wrapper.find({ children: 'Error component' });
 
   return {
-    wrapper,
-    globeComponent,
-    loadingComponent,
-    errorComponent
+    wrapper
   };
 };
 
 describe('GlobeWrapper component', () => {
-  describe('isLoading is true', () => {
-    it('should render only loading indicator', () => {
-      const { loadingComponent, globeComponent, errorComponent } = setup();
-      expect(loadingComponent.exists()).toBe(true);
-      expect(globeComponent.exists()).toBe(false);
-      expect(errorComponent.exists()).toBe(false);
-    });
+  it('should render', () => {
+    const { wrapper } = setup();
+    expect(wrapper.exists()).toBe(true);
   });
+});
 
-  describe('isLoading is false and there is an error', () => {
-    it('should render only error component', () => {
-      const { loadingComponent, globeComponent, errorComponent } = setup({
-        isLoading: false,
-        error: Error('Error found')
-      });
-      expect(errorComponent.exists()).toBe(true);
-      expect(loadingComponent.exists()).toBe(false);
-      expect(globeComponent.exists()).toBe(false);
-    });
-  });
+describe('mapStateToProps', () => {
+  it('should return correct data', () => {
+    const testInitialState = {
+      ...initialState,
+      locations: {
+        ...initialState.locations,
+        coords: [25, 25]
+      }
+    };
 
-  describe('isLoading is false and there is no error', () => {
-    it('should render only globe component', () => {
-      const { loadingComponent, globeComponent, errorComponent } = setup({
-        isLoading: false,
-        error: null
-      });
-      expect(globeComponent.exists()).toBe(true);
-      expect(errorComponent.exists()).toBe(false);
-      expect(loadingComponent.exists()).toBe(false);
-    });
-  });
-
-  describe('mapStateToProps', () => {
-    it('should return correct data', () => {
-      const testInitialState = {
-        ...initialState,
-        locations: {
-          coords: [25, 25]
-        }
-      };
-
-      expect(mapStateToProps(testInitialState).isLoading).toBe(true);
-      expect(mapStateToProps(testInitialState).error).toBe(null);
-      expect(mapStateToProps(testInitialState).data.coords).toStrictEqual([
-        25,
-        25
-      ]);
-    });
+    expect(mapStateToProps(testInitialState).data.coords).toStrictEqual([
+      25,
+      25
+    ]);
   });
 });
