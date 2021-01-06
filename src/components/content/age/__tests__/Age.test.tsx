ad89107da71d9ba56caae232dea952a4bd731085
IgorOluic <igoroluic13@gmail.com>
2021-01-06 18:26:03 +0100

    Update Age component tests

--- src/components/content/age/__tests__/Age.test.tsx
+++ src/components/content/age/__tests__/Age.test.tsx
@@ -1,65 +1,28 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { LinearProgress } from '@material-ui/core';
 import { mapStateToProps } from '../AgeContainer';
 import Age from '../Age';
 import initialState from '../../../../state/initialState';
 
-const setup = (propOverrides?: { isLoading: boolean; error: Error }) => {
+const setup = () => {
   const props = {
     data: {
-      age: 22
-    },
-    isLoading: true,
-    error: null,
-    ...propOverrides
+      age: 22,
+      name: 'John'
+    }
   };
 
   const wrapper = shallow(<Age {...props} />);
-  const ageComponent = wrapper.find({ children: 22 });
-  const loadingComponent = wrapper.find(LinearProgress);
-  const errorComponent = wrapper.find({ children: 'Error component' });
 
   return {
-    wrapper,
-    ageComponent,
-    loadingComponent,
-    errorComponent
+    wrapper
   };
 };
 
 describe('Age component', () => {
-  describe('isLoading is true', () => {
-    it('should render only loading indicator', () => {
-      const { loadingComponent, ageComponent, errorComponent } = setup();
-      expect(loadingComponent.exists()).toBe(true);
-      expect(ageComponent.exists()).toBe(false);
-      expect(errorComponent.exists()).toBe(false);
-    });
-  });
-
-  describe('isLoading is false and there is an error', () => {
-    it('should render only error component', () => {
-      const { loadingComponent, ageComponent, errorComponent } = setup({
-        isLoading: false,
-        error: Error('Error found')
-      });
-      expect(loadingComponent.exists()).toBe(false);
-      expect(ageComponent.exists()).toBe(false);
-      expect(errorComponent.exists()).toBe(true);
-    });
-  });
-
-  describe('isLoading is false and there is no error', () => {
-    it('should render only age component', () => {
-      const { loadingComponent, ageComponent, errorComponent } = setup({
-        isLoading: false,
-        error: null
-      });
-      expect(loadingComponent.exists()).toBe(false);
-      expect(ageComponent.exists()).toBe(true);
-      expect(errorComponent.exists()).toBe(false);
-    });
+  it('should render', () => {
+    const { wrapper } = setup();
+    expect(wrapper.exists()).toBe(true);
   });
 
   describe('mapStateToProps', () => {
@@ -70,14 +33,14 @@ describe('Age component', () => {
           ...initialState.content,
           nameInfo: {
             ...initialState.content.nameInfo,
-            age: 22
+            age: 22,
+            name: 'John'
           }
         }
       };
 
-      expect(mapStateToProps(testInitialState).isLoading).toBe(true);
-      expect(mapStateToProps(testInitialState).error).toBe(null);
       expect(mapStateToProps(testInitialState).data.age).toBe(22);
+      expect(mapStateToProps(testInitialState).data.name).toBe('John');
     });
   });
 });
